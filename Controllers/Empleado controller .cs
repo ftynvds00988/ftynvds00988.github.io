51e94e836f721e112cea98aca835ab080305524e
ftynvds00988 <114834355+ftynvds00988@users.noreply.github.com>
2022-10-01 19:46:23 -0300

    Subiendo la pagina web

---
+++ Controllers/Empleado controller .cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistroNuevo.UIWebMVC.Controllers
+{
+    public class Empleado_controller
+    {
+        // GET: Empleado 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult BuscarEmpleadoPorId(int Id)
+        {
+            byte id = Convert.ToByte(Id);
+            return JsonResult(EmpleadoBL.BuscarPorId(id), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Guardar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Guardar(Empleado empleado)
+        {
+            string mensaje = "";
+            if (empleado.Nombre != "" && empleado != null)
+            {
+                if (EmpleadoBL.Guardar(empleado) > 0)
+                {
+                    mensaje = "Registro Guardado";
+                }
+                else
+                {
+                    mensaje = "No se Guardo el Registro";
+                }
+            }
+            else
+            {
+                mensaje = "No dejar campos vacios";
+            }
+            return Json(new { Mensaje = mensaje, JsonRequestBehavior.AllowGet });
+        }
+        //Json: JSON (Notación de objetos de JavaScript) 
+        //JsonResult: Un resultado de acción que formatea el objeto dado como JSON.
+        public ActionResult Modificar(byte id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Modificar(Empleado empleado)
+        {
+            string mensaje = "";
+            if (empleado.Nombre != "" && empleado != null)
+            {
+                if (EmpleadoBL.Modificar(empleado) > 0)
+                {
+                    mensaje = "Registro Actualizado";
+                }
+                else
+                {
+                    mensaje = "No se Actualizó el Registro";
+                }
+            }
+            else
+            {
+                mensaje = "No dejar campos vacios";
+            }
+            return Json(new { Mensaje = mensaje, JsonRequestBehavior.AllowGet });
+        }
+
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Eliminar(Empleado empleado)
+        {
+            string mensaje = "";
+            if (empleado.Nombre != "" && empleado != null)
+            {
+                if (EmpleadoBL.Eliminar(empleado) > 0)
+                {
+                    mensaje = "Registro Eliminado";
+                }
+                else
+                {
+                    mensaje = "No se Eliminó el Registro";
+                }
+            }
+            else
+            {
+                mensaje = "No dejar campos vacios";
+            }
+            return Json(new { Mensaje = mensaje, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
+   
+    
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("RegistroNuevo.UIWebMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegistroNuevo.UIWebMVC")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("dcf9941f-d6a4-4304-9b87-5f71009d8a6c")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
